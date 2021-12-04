9540803bb4479016151b184072cd29705e205868
Eric Grochowski <egrochowski42@gmail.com>
2021-12-03 23:32:45 -0500

    Change to typescript file extension

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`listening at port ${PORT}...`);
+});
