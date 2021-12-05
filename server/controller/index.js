b8657358fedd803c59ba88457a4c468ab1cd6e6d
Eric Grochowski <egrochowski42@gmail.com>
2021-12-05 11:50:43 -0500

    Add request handler to save universe and make fix to get request

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,7 +1,12 @@
 const model = require("./../model/");
 
 module.exports = {
-  getPresets: (req, res) => {
-    res.send(model.getPresets());
+  getPresets: async (req, res) => {
+    res.send(await model.getPresets());
+  },
+
+  addUniverse: (req, res) => {
+    model.addUniverse(req.body);
+    res.sendStatus(201);
   },
 };
