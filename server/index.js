4df8aef54e2f04e9b1b9c1790650c02fedf7c131
Eric Grochowski <egrochowski42@gmail.com>
2021-12-05 17:18:02 -0500

    Update order of execution

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ const routes = require("./routes.js");
 const database = require("./db");
 const app = express();
 
-app.use(morgan("dev"));
-app.use(routes);
-app.use(express.json());
 app.use(express.static(path.join(__dirname, "./../public")));
+app.use(express.json());
+app.use(routes);
+app.use(morgan("dev"));
 
 const PORT = process.env.PORT || 3000;
 
