c44a5a2fef6f39b10901236823113ef22550a32c
Eric Grochowski <egrochowski42@gmail.com>
2021-12-05 11:56:14 -0500

    Add post route to save universes

--- server/routes.js
+++ server/routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const controller = require("./controller");
 
 router.get("/presets", controller.getPresets);
+router.post("/presets", controller.addUniverse);
 
 module.exports = router;
