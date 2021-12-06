b325a14439eba9cfc8abaa9f7eeb1b751dd27eba
Eric Grochowski <egrochowski42@gmail.com>
2021-12-06 00:30:03 -0500

    Create about component

---
+++ client/src/components/about.tsx
@@ -0,0 +1,16 @@
+import * as React from "React";
+
+interface Count {
+  generations: number;
+}
+
+const About: React.FC<Count> = ({ generations }) => (
+  <div className="about">
+    <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">
+      About Conway's Game of Life
+    </a>
+    <p>Generations: {generations}</p>
+  </div>
+);
+
+export default About;
