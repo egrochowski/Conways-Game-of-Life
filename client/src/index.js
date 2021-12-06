bfacb9da82e1cf75c72f2173c8e68d1ead3bf60d
Eric Grochowski <egrochowski42@gmail.com>
2021-12-06 01:12:03 -0500

    Import and render app component

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Universe from './universe.tsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app.tsx";
 
-ReactDOM.render(<Universe />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
