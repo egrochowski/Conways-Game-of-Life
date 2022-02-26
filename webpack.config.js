105a00e6048314b82f533701a6688a55fdde886f
Eric Grochowski <egrochowski42@gmail.com>
2022-02-26 12:17:06 -0500

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,59 @@
-const path = require("path");
+// const path = require('path');
 
-module.exports = {
-  entry: "./client/src/index.js",
+// module.exports = {
+//   entry: './client/src/index.tsx',
+//   output: {
+//     path: path.resolve(__dirname, 'public'),
+//     filename: 'bundle.js',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(ts|tsx)$/,
+//         loader: 'ts-loader',
+//         exclude: [/node_modules/, /bower_components/],
+//       },
+//       {
+//         test: /.jsx?$/,
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//       },
+//     ],
+//   },
+//   mode: 'production',
+// };
+
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: ['./client/src/index.tsx'],
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)$/,
-        loader: "ts-loader",
-        exclude: [/node_modules/, /bower_components/],
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
       },
       {
-        test: /.jsx?$/,
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
         exclude: /node_modules/,
-        loader: "babel-loader",
       },
     ],
   },
-  mode: "production",
+  devServer: {
+    static: {
+      directory: './public',
+    },
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
 };
+
+module.exports = config;
