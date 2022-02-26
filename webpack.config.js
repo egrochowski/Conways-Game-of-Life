27bd46861b509e83af2da22b1033a2b7ac4a4469
Eric Grochowski <egrochowski42@gmail.com>
2022-02-26 15:40:41 -0500

    Remove comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,3 @@
-// const path = require('path');
-
-// module.exports = {
-//   entry: './client/src/index.tsx',
-//   output: {
-//     path: path.resolve(__dirname, 'public'),
-//     filename: 'bundle.js',
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(ts|tsx)$/,
-//         loader: 'ts-loader',
-//         exclude: [/node_modules/, /bower_components/],
-//       },
-//       {
-//         test: /.jsx?$/,
-//         exclude: /node_modules/,
-//         loader: 'babel-loader',
-//       },
-//     ],
-//   },
-//   mode: 'production',
-// };
-
-const webpack = require('webpack');
 const path = require('path');
 
 const config = {
