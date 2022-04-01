bea3c346e6c0184d446193ddc4777812ddd1fee7
Eric Grochowski <egrochowski42@gmail.com>
2022-04-01 16:21:59 -0400

    Add route to get universes

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
+router.get('/universes', controller.getAll);
 router.get('/presets', controller.getPresets);
 router.get('/userSaves', controller.getUserSaves);
 router.post('/presets', controller.addUniverse);
