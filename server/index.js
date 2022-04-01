3856fdb27c3e9d27753ab6269ec0d40416012489
Eric Grochowski <egrochowski42@gmail.com>
2022-04-01 12:29:25 -0400

    Change execution order of middleware

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
-const routes = require("./routes.js");
-const database = require("./db");
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const routes = require('./routes.js');
 const app = express();
 
-app.use(express.static(path.join(__dirname, "./../public")));
 app.use(express.json());
+app.use(morgan('dev'));
 app.use(routes);
-app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, './../public')));
 
 const PORT = process.env.PORT || 3000;
 
