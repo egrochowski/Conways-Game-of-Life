d4266a8335d3b8d409056a197c3274117198455d
Eric Grochowski <egrochowski42@gmail.com>
2022-04-01 12:31:02 -0400

    Remove function to handle successful connection to the database

--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,12 @@
-const client = require("mongodb");
-const mongoose = require("mongoose");
-const URI = "mongodb://localhost/game-of-life";
+const mongoose = require('mongoose');
+const URI = 'mongodb://localhost/game-of-life';
 
 mongoose.connect(URI);
 
 const db = mongoose.connection;
 
-db.on("error", () => {
-  console.log("Error connecting to database");
-});
-
-db.once("open", () => {
-  console.log("successfully connected to database");
+db.on('error', () => {
+  console.log('Error connecting to database');
 });
 
 const Universes = new mongoose.Schema(
@@ -25,4 +20,4 @@ const Universes = new mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model("universes", Universes);
+module.exports = mongoose.model('universes', Universes);
