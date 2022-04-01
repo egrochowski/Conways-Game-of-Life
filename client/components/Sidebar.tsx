c840775427984040d6497cc25cbd5271a3a51406
Eric Grochowski <egrochowski42@gmail.com>
2022-04-01 17:40:22 -0400

    Implement function to render list for user saves and presets

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -1,36 +1,45 @@
 import * as React from 'react';
 
-interface Presets {
+interface Universe {
   createdAt: String;
   name: String;
   preset: boolean;
   universe: number[][];
 }
 
-type PresetsType = {
-  presets: Presets[];
-  handleNewPreset(index: number): void;
+type UniverseType = {
+  presets: Universe[];
+  userSaves: Universe[];
+  handleNewUniverse(preset: boolean, index: number): void;
 };
 
-const Sidebar: React.FC<PresetsType> = ({ presets, handleNewPreset }) => (
-  <div className='sidebar'>
-    <div className='presets-header'>Presets</div>
-    <div className='preset-container'>
-      {presets.map((universe, index) => (
-        <span
-          key={index}
-          className='preset'
-          onClick={() => {
-            handleNewPreset(index);
-          }}
-        >
-          {universe.name}
-        </span>
-      ))}
+const Sidebar: React.FC<UniverseType> = ({
+  presets,
+  userSaves,
+  handleNewUniverse,
+}) => {
+  const displayUniverses = (universes: Universe[]) => {
+    return universes.map((universe, index) => (
+      <span
+        key={index}
+        className='preset'
+        onClick={() => {
+          handleNewUniverse(universe.preset, index);
+        }}
+      >
+        {universe.name}
+      </span>
+    ));
+  };
+
+  return (
+    <div className='sidebar'>
+      <div className='presets-header'>Presets</div>
+      <div className='preset-container'>{displayUniverses(presets)}</div>
+      <div className='presets-header'>User Saves</div>
+      <div className='preset-container'>{displayUniverses(userSaves)}</div>
     </div>
-    <div className='presets-header'>User Saves</div>
-    <div className='preset-container'></div>
-  </div>
-);
+  );
+};
 
 export default Sidebar;
