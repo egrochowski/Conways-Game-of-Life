0ffc51c77f9433f495e64f1621c3845a8ee0c259
Eric Grochowski <egrochowski42@gmail.com>
2022-04-02 16:25:38 -0400

    Removes routes.js

--- server/routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const controller = require('./controller');
-
-router.get('/universes', controller.getAll);
-router.get('/presets', controller.getPresets);
-router.get('/userSaves', controller.getUserSaves);
-router.post('/universe', controller.addUniverse);
-
-module.exports = router;
