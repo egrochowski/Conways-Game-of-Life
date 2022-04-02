084abfd530bb1f178d0f9f4e9772f52bf8a10aab
Eric Grochowski <egrochowski42@gmail.com>
2022-04-01 23:41:24 -0400

    Modify route to post to universe endpoint

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,6 @@ const controller = require('./controller');
 router.get('/universes', controller.getAll);
 router.get('/presets', controller.getPresets);
 router.get('/userSaves', controller.getUserSaves);
-router.post('/presets', controller.addUniverse);
+router.post('/universe', controller.addUniverse);
 
 module.exports = router;
