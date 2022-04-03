9144f490c38627c8e8a4e1df10ad7c9e560c03b5
Eric Grochowski <egrochowski42@gmail.com>
2022-04-03 12:42:32 -0400

    Create webpack configurations

---
+++ client/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const config = {
+  entry: './index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  mode: 'production',
+};
+
+module.exports = config;
