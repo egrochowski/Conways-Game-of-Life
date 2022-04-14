65dfab2d9e3cafcdf6ab053ceda341c3ae9b6034
Eric Grochowski <egrochowski42@gmail.com>
2022-04-14 11:59:48 -0400

    Move files and directories into src folder

--- client/Interfaces/IActions.ts
+++
@@ -1,8 +0,0 @@
-export interface IActions {
-  onPlayStop(): void;
-  onChange(e: React.ChangeEvent<HTMLInputElement>): void;
-  onReset(): void;
-  onClear(): void;
-  onSave(): void;
-  isRunning: boolean;
-}
--- client/types/universe.ts
+++
@@ -1,7 +0,0 @@
-import IUniverse from './../Interfaces/IUniverse';
-
-export type Universe = {
-  presets: IUniverse[];
-  userSaves: IUniverse[];
-  handleNewUniverse(preset: boolean, index: number): void;
-};
